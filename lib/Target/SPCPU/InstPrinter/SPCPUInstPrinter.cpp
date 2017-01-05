591fe6e529f0c860adf75a3318383c8265adbaf1
fl4shk <fl4shk@users.noreply.github.com>
2017-01-04 18:09:40 -0600

    Update SPCPU to Xarch again

--- lib/Target/SPCPU/InstPrinter/SPCPUInstPrinter.cpp
+++ lib/Target/SPCPU/InstPrinter/SPCPUInstPrinter.cpp
@@ -84,8 +84,6 @@ void SPCPUInstPrinter::printAddrModeMemSrc(const MCInst *MI, unsigned OpNum,
 
 void SPCPUInstPrinter::printOperand(const MCInst *MI, unsigned OpNo,
                                   raw_ostream &O) {
-  // XXX: Add once there is an operand encoding available
-  return;
   const MCOperand &Op = MI->getOperand(OpNo);
   if (Op.isReg()) {
     printRegName(O, Op.getReg());
